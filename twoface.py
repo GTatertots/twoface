bea8835f5bef7e6d118110f09fa2b2236e7e5849
root <root@DESKTOP-OVBT6VQ.localdomain>
2024-02-20 15:29:20 -0700

    mostly implemented click on current functions

--- twoface.py
+++ twoface.py
@@ -1,79 +1,116 @@
 #!/usr/bin/env python3
 
-import argparse
+import click
+import os
 import sqlite3
+import sys
+
+DB_FILE = 'twoface.db'
+
+def getdb(create=False):
+    if os.path.exists(DB_FILE):
+        if create:
+            os.remove(DB_FILE)
+    else:
+        if not create:
+            print('no database found')
+            sys.exit(1)
+    con = sqlite3.connect(DB_FILE)
+    con.execute('PRAGMA foreign_keys = ON')
+    return con
 
 
+@click.group()
+def cli():
+    pass
 
-def insert_user(database_name, email_addr):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO users (email_address) VALUES (?)", (email_addr))
-    conn.commit()
-    conn.close()
+@click.command()
+@click.argument('email_addr')
+def insert_user(email_addr):
+    with getdb() as con:
+        c = con.cursor()
+        c.execute("INSERT INTO users (email_address) VALUES (?)", (email_addr))
+        conn.commit()
+        conn.close()
 
+@click.command()
+@click.argument('username')
+@click.argument('email_addr')
+def insert_account(username, email_addr): 
+    with getdb() as con:
+        c = con.cursor()
+        c.execute("INSERT INTO accounts (username, email_address) VALUES (?, ?)", (username, email_addr))
+        conn.commit()
+        conn.close()
 
-def insert_account(database_name, username, email_addr, account_id):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO accounts (username, email_address, account_id) VALUES (?, ?, ?)", (username, email_addr, account_id))
-    conn.commit()
-    conn.close()
+@click.command()
+@click.argument('follower')
+@click.argument('followed')
+def insert_follower(follower, followed): 
+    with getdb() as con: 
+        c = con.cursor()
+        #TODO needs to be fixed
+        c.execute("INSERT INTO followers (follower, followed) VALUES (?, ?)", (follower, followed))
+        conn.commit()
+        conn.close()
 
-def insert_follower(database_name, follower, followed):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO followers (follower, followed) VALUES (?, ?)", (follower, followed))
-    conn.commit()
-    conn.close()
+@click.command()
+@click.argument('username')
+@click.argument('message')
+@click.argument('year')
+@click.argument('month')
+@click.argument('day')
+@click.argument('hour')
+@click.argument('minute')
+def insert_post(username, message, year, month, day, hour, minute):
+    with getdb() as con: 
+        c = con.cursor()
+        c.execute("INSERT INTO posts (message, poster_id, year, month, day, hour, minute) VALUES (?, ?, ?, ?, ?, ?, ?)", (message, poster_id, year, month, day, hour, minute))
+        conn.commit()
+        conn.close()
 
-def insert_post(database_name, message, poster_id, year, month, day, hour, minute):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO posts (message, poster_id, year, month, day, hour, minute) VALUES (?, ?, ?, ?, ?, ?, ?)", (message, poster_id, year, month, day, hour, minute))
-    conn.commit()
-    conn.close()
+@click.command()
+@click.argument('')
+def insert_reply(post_id, message, replier_id, year, month, day, hour, minute): 
+    with getdb() as con: 
+        c = con.cursor()
+        c.execute("INSERT INTO replies (message, post_id, replier_id, year, month, day, hour, minute) VALUES (?, ?, ?, ?, ?, ?, ?)", (message, post_id, replier_id, year, month, day, hour, minute))
+        conn.commit()
+        conn.close()
 
-def insert_follower(database_name, poster_id, liker_id):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO followers (poster_id, liker_id) VALUES (?, ?)", (poster_id, liker_id))
-    conn.commit()
-    conn.close()
-
-def insert_reply(database_name, post_id, message, replier_id, year, month, day, hour, minute):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO replies (message, post_id, replier_id, year, month, day, hour, minute) VALUES (?, ?, ?, ?, ?, ?, ?)", (message, post_id, replier_id, year, month, day, hour, minute))
-    conn.commit()
-    conn.close()
-
-def insert_like(database_name, post_id, liker_id):
-    conn = sqlite3.connect(database_name)
-    c = conn.cursor()
-    c.execute("INSERT INTO likes (post_id, liker_id) VALUES (?, ?)", (post_id, liker_id))
-    conn.commit()
-    conn.close()
-
-def error(args):
-    print('This command requires a subcommand')
-
-
-def main():
-    parser = argparse.ArgumentParser(
-            prog = 'TwoFace Interface',
-            description = 'UI for TwoFace social network')
-    subparsers = parser.add_subparsers(help='sub-command help')
-
-    parser_insert_user = subparsers.add_parser('insertuser',help='adds a user')
-    parser_insert_user.add_argument('email')
-    parser_insert_user.set_defaults(func=add_user)
-    args = parser.parse_args()
-
-    print(args,filename,args.count,args.verbose)
-
+@click.command()
+@click.argument('')
+def insert_like(post_id, liker_id):
+    with getdb() as con: 
+        c = conn.cursor()
+        c.execute("INSERT INTO likes (post_id, liker_id) VALUES (?, ?)", (post_id, liker_id))
+        conn.commit()
+        conn.close()
 
 
+cli.add_command(insert_user)
+cli.add_command(insert_account)
+cli.add_command(insert_follower)
+cli.add_command(insert_post)
+cli.add_command(insert_reply)
+cli.add_command(insert_like)
+cli()
+
+#def main():
+#    parser = argparse.ArgumentParser(
+#            prog = 'TwoFace Interface',
+#            description = 'UI for TwoFace social network')
+#    subparsers = parser.add_subparsers(help='sub-command help')
+#
+#    parser_insert_user = subparsers.add_parser('insertuser',help='adds a user')
+#    parser_insert_user.add_argument('email')
+#    parser_insert_user.set_defaults(func=add_user)
+#    args = parser.parse_args()
+#
+#    print(args,filename,args.count,args.verbose)
+#
+#
+#
 #import click
 #import os
 #import sqlite3
@@ -98,27 +135,6 @@ def main():
 #    pass
 #
 #@click.command()
-#def create():
-#    with getdb(create=True) as con:
-#        con.execute(
-#'''CREATE TABLE users (
-#    id          INTEGER PRIMARY KEY,
-#    email       TEXT NOT NULL
-#)''')
-#        con.execute(
-#'''CREATE UNIQUE INDEX users_email ON users (email)''')
-#
-#        con.execute(
-#'''CREATE TABLE accounts (
-#    id          INTEGER PRIMARY KEY,
-#    user_id     INTEGER NOT NULL,
-#    username    TEXT NOT NULL,
-#
-#    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
-#)''')
-#    print('database created')
-#
-#@click.command()
 #@click.argument('email')
 #def adduser(email):
 #    print('creating user with email address', email)
@@ -140,24 +156,8 @@ def main():
 #        id = cursor.lastrowid
 #        print(f'inserted with id={id}')
 #
-#cli.add_command(create)
+#
+#
 #cli.add_command(adduser)
 #cli.add_command(addaccount)
 #cli()
-##import sqlite3
-##
-##con = sqlite3.connect("twoface.db")
-##cur = con.cursor()
-##
-##def main():
-##    parser = argparse.ArgumentParser(
-##            prog = 'TwoFace Interface',
-##            description = 'UI for TwoFace social network')
-##    subparsers = parser.add_subparsers(help='sub-command help')
-##
-##    parser_create = subparsers.add_parser('create',help='create the database')
-##
-##    args = parser.parse_args()
-##
-##    print(args,filename,args.count,args.verbose)
-#

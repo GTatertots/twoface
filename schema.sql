93c9a3c6039425bebc86a5b3094d96ce79aecc14
Ryan Crandall <Ryan.Crandall@utahtech.edu>
2024-02-21 19:55:03 -0700

    OOOP

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE accounts (
-	account_id INT PRIMARY KEY,
+	account_id INTEGER PRIMARY KEY,
 	email_address TEXT NOT NULL,
 	username TEXT NOT NULL UNIQUE,
 	FOREIGN KEY (email_address) REFERENCES users(email_address)
@@ -13,8 +13,8 @@ CREATE TABLE accounts (
 );
 
 CREATE TABLE followers (
-	follower_id INT NOT NULL,
-	followed_id INT NOT NULL,
+	follower_id INTEGER NOT NULL,
+	followed_id INTEGER NOT NULL,
 	PRIMARY KEY (follower_id, followed_id),
 	FOREIGN KEY (follower_id) REFERENCES accounts(account_id)
 		ON DELETE CASCADE
@@ -25,22 +25,22 @@ CREATE TABLE followers (
 );
 
 CREATE TABLE posts (
-	post_id INT PRIMARY KEY,
+	post_id INTEGER PRIMARY KEY,
 	message TEXT NOT NULL, 
-	poster_id INT NOT NULL,
-	year INT,
-	month INT check(month BETWEEN 1 AND 12),
-	day INT check(day BETWEEN 1 AND 31), 
-	hour INT check(hour BETWEEN 0 AND 23),
-	minute INT check(minute BETWEEN 0 and 59),
+	poster_id INTEGER NOT NULL,
+	year INTEGER,
+	month INTEGER check(month BETWEEN 1 AND 12),
+	day INTEGER check(day BETWEEN 1 AND 31), 
+	hour INTEGER check(hour BETWEEN 0 AND 23),
+	minute INTEGER check(minute BETWEEN 0 and 59),
 	FOREIGN KEY (poster_id) REFERENCES accounts(account_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
 
 CREATE TABLE likes (
-	post_id INT,
-	liker_id INT,
+	post_id INTEGER,
+	liker_id INTEGER,
 	PRIMARY KEY(post_id, liker_id),
 	FOREIGN KEY (liker_id) REFERENCES accounts(account_id)
 		ON DELETE NO ACTION
@@ -51,18 +51,18 @@ CREATE TABLE likes (
 );
 
 CREATE TABLE replies (
-	reply_id INT PRIMARY KEY,
-	post_id INT ,
+	reply_id INTEGER PRIMARY KEY,
+	post_id INTEGER ,
 	message TEXT NOT NULL, 
-	replier_id INT,
+	replier_id INTEGER,
 	parent_reply_id INT REFERENCES accounts(reply_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
-	year INT,
-	month INT check(month BETWEEN 1 AND 12),
-	day INT check(day BETWEEN 1 AND 31), 
-	hour INT check(hour BETWEEN 0 AND 23),
-	minute INT check(minute BETWEEN 0 AND 59),
+	year INTEGER,
+	month INTEGER check(month BETWEEN 1 AND 12),
+	day INTEGER check(day BETWEEN 1 AND 31), 
+	hour INTEGER check(hour BETWEEN 0 AND 23),
+	minute INTEGER check(minute BETWEEN 0 AND 59),
 	FOREIGN KEY (replier_id) REFERENCES accounts(account_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,

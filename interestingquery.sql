1a3273f14b64cdd89c7fbb77c1a709b125ccf57d
Ryan Crandall <Ryan.Crandall@utahtech.edu>
2024-02-22 18:09:49 -0700

    fixed all the interesting queries

--- interestingquery.sql
+++ interestingquery.sql
@@ -1,14 +1,13 @@
 SELECT A.username AS greatest_liker, count(1) AS like_amount
-FROM accounts,
-WHERE accounts.username = ?,
+FROM accounts
 JOIN posts ON
-	poster_id == account_id,
+	poster_id == accounts.account_id
 JOIN likes ON 
-	likes.post_id == posts.post_id,
+	likes.post_id == posts.post_id
 JOIN accounts AS A ON 
-	liker_id == A.account_id,
-GROUP BY liker_id,
-ORDER BY like_amount DESC
+	liker_id == A.account_id
+GROUP BY liker_id
+ORDER BY like_amount DESC;
 
 SELECT posts.message, count(1) AS likes
 FROM accounts 
@@ -22,11 +21,11 @@ GROUP BY likes.post_id
 ORDER BY year, month, day, hour, minute DESC;
 
 SELECT accounts.username, count(1) as follower_count
-FROM followers,
+FROM followers
 JOIN accounts ON 
-	followed_id == account_id,
-GROUP BY followed_id,
-ORDER BY follower_amount DESC,
+	followed_id == account_id
+GROUP BY followed_id
+ORDER BY follower_count DESC
 LIMIT 10;
 
 SELECT replies.message, count(1) AS likes
